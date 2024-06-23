4633d804c16a98d95baac18e1e5d0a1c6cdee988
Long T. Kira <longhg04515@gmail.com>
2024-06-23 22:32:43 +0700

    okay

--- cluster-command.sh
+++ cluster-command.sh
@@ -1,3 +1,3 @@
-docker start atomix-1 atomix-2 atomix-3 atomix-4 onos-1 onos-2 onos-3 onos-4
-docker stop atomix-1 atomix-2 atomix-3 atomix-4 onos-1 onos-2 onos-3 onos-4
+docker start atomix-1 atomix-2 atomix-3 onos-1 onos-2 onos-3
+docker stop atomix-1 atomix-2 atomix-3 onos-1 onos-2 onos-3
 docker restart atomix-1 atomix-2 atomix-3 atomix-4 onos-1 onos-2 onos-3 onos-4
